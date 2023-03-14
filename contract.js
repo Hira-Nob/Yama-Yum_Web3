794d65bdc243a2846f0344f2d7745a923dd9f1e0
tmz264 <90554835+tmz264@users.noreply.github.com>
2023-03-14 23:54:15 +0900

    Update contract.js

--- contract.js
+++ contract.js
@@ -10,12 +10,19 @@ const initialize = () => {
     let myContract;
     
     
-          async function loadWeb3(){
+   async function loadWeb3(){
    if(window.ethereum) {
     window.web3 = new Web3(window.ethereum);
     window.ethereum.enable();
    }
   }
+    
+      // 利用者のメタマスクのウォレットアドレスを取得
+  async function getCurrentAccount(){
+   const accounts = await window.web3.eth.getAccounts();
+   return accounts[0];
+  }
+
     
     //enter deployed contract abi
     const ContractAbi = [
